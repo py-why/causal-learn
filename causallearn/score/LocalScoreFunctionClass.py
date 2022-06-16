a0db10a5e5d35b04ee2a0fcbf34c3110797a1c76
bja43 <bja43@pitt.edu>
2022-06-16 01:28:03 -0400

    added BIC from cov and GRaSP

--- causallearn/score/LocalScoreFunctionClass.py
+++ causallearn/score/LocalScoreFunctionClass.py
@@ -1,24 +1,35 @@
 import math
 from functools import lru_cache
-from typing import List, Dict, Any, Callable
+from typing import Any, Callable, Dict, List
 
 import pandas as pd
-from numpy import ndarray
-from causallearn.score.LocalScoreFunction import local_score_BDeu, local_score_BIC, local_score_cv_multi, local_score_marginal_multi, local_score_marginal_general, local_score_cv_general
-
+from causallearn.score.LocalScoreFunction import (
+    local_score_BDeu,
+    local_score_BIC,
+    local_score_BIC_from_cov,
+    local_score_cv_general,
+    local_score_cv_multi,
+    local_score_marginal_general,
+    local_score_marginal_multi,
+)
 from causallearn.utils.ScoreUtils import *
+from numpy import ndarray
 
 
 class LocalScoreClass(object):
-
-    def __init__(self, data: ndarray, local_score_fun: Callable[[ndarray, int, List[int], Any], float], parameters=None):
+    def __init__(
+        self,
+        data: Any,
+        local_score_fun: Callable[[Any, int, List[int], Any], float],
+        parameters=None,
+    ):
         self.data = data
         self.local_score_fun = local_score_fun
         self.parameters = parameters
         self.score_cache = {}
 
     def score(self, i: int, PAi: List[int]) -> float:
-        hash_key = f'i_{str(i)}_PAi_{str(PAi)}'
+        hash_key = f"i_{str(i)}_PAi_{str(PAi)}"
         if self.score_cache.__contains__(hash_key):
             return self.score_cache[hash_key]
         else:
