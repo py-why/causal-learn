295cee120cf6bbc0ed2acfca7f234c0f1d42e082
aoqiz <aoqiz0021@gmail.com>
2022-07-06 18:12:21 +0800

    Fix TestCIT_KCI.py

--- tests/TestCIT_KCI.py
+++ tests/TestCIT_KCI.py
@@ -4,6 +4,7 @@ import numpy as np
 
 import causallearn.utils.cit as cit
 
+
 class TestCIT_KCI(unittest.TestCase):
     def test_Gaussian(self):
         X = np.random.randn(300, 1)
@@ -11,16 +12,16 @@ class TestCIT_KCI(unittest.TestCase):
         Y = np.concatenate((X, X), axis=1) + 0.5 * np.random.randn(300, 2)
         Z = Y + 0.5 * np.random.randn(300, 2)
 
-        cit_CIT = cit.CIT(data=np.hstack((X,X1,Y,Z)), method='kci')
+        cit_CIT = cit.CIT(data=np.hstack((X, X1, Y, Z)), method='kci')
 
-        pvalue = cit_CIT.kci(0, 1, {})
-        self.assertTrue(pvalue > 0.01) # X and X1 are independent
+        pvalue = cit_CIT(0, 1)
+        self.assertTrue(pvalue > 0.01)  # X and X1 are independent
 
-        pvalue = cit_CIT.kci(0, 3, {})
-        self.assertTrue(pvalue <= 0.01) # X and Z are dependent
+        pvalue = cit_CIT(0, 4)
+        self.assertTrue(pvalue <= 0.01)  # X and Z are dependent
 
-        pvalue = cit_CIT.kci(0, 3, {2})
-        self.assertTrue(pvalue > 0.01) # X and Z are independent conditional on Y
+        pvalue = cit_CIT(0, 4, {2})
+        self.assertTrue(pvalue > 0.01)  # X and Z are independent conditional on Y
 
     def test_Polynomial(self):
         X = np.random.randn(300, 1)
@@ -28,16 +29,17 @@ class TestCIT_KCI(unittest.TestCase):
         Y = np.concatenate((X, X), axis=1) + 0.5 * np.random.randn(300, 2)
         Z = Y + 0.5 * np.random.randn(300, 2)
 
-        cit_CIT = cit.CIT(data=np.hstack((X,X1,Y,Z)), method='kci', kernelX='Polynomial', kernelY='Polynomial', kernelZ='Polynomial')
+        cit_CIT = cit.CIT(data=np.hstack((X, X1, Y, Z)), method='kci', kernelX='Polynomial', kernelY='Polynomial',
+                          kernelZ='Polynomial')
 
-        pvalue = cit_CIT.kci(0, 1, {})
-        self.assertTrue(pvalue > 0.01) # X and X1 are independent
+        pvalue = cit_CIT(0, 1)
+        self.assertTrue(pvalue > 0.01)  # X and X1 are independent
 
-        pvalue = cit_CIT.kci(0, 3, {})
-        self.assertTrue(pvalue <= 0.01) # X and Z are dependent
+        pvalue = cit_CIT(0, 4)
+        self.assertTrue(pvalue <= 0.01)  # X and Z are dependent
 
-        pvalue = cit_CIT.kci(0, 3, {2})
-        self.assertTrue(pvalue > 0.01) # X and Z are independent conditional on Y
+        pvalue = cit_CIT(0, 4, {2})
+        self.assertTrue(pvalue > 0.01)  # X and Z are independent conditional on Y
 
     def test_Linear(self):
         X = np.random.randn(300, 1)
@@ -45,16 +47,18 @@ class TestCIT_KCI(unittest.TestCase):
         Y = np.concatenate((X, X), axis=1) + 0.5 * np.random.randn(300, 2)
         Z = Y + 0.5 * np.random.randn(300, 2)
 
-        cit_CIT = cit.CIT(data=np.hstack((X,X1,Y,Z)), method='kci', kernelX='Linear', kernelY='Linear', kernelZ='Linear')
+        cit_CIT = cit.CIT(data=np.hstack((X, X1, Y, Z)), method='kci', kernelX='Linear', kernelY='Linear',
+                          kernelZ='Linear')
 
-        pvalue = cit_CIT.kci(0, 1, {})
-        self.assertTrue(pvalue > 0.01) # X and X1 are independent
+        pvalue = cit_CIT(0, 1)
+        self.assertTrue(pvalue > 0.01)  # X and X1 are independent
 
-        pvalue = cit_CIT.kci(0, 3, {})
-        self.assertTrue(pvalue <= 0.01) # X and Z are dependent
+        pvalue = cit_CIT(0, 4)
+        self.assertTrue(pvalue <= 0.01)  # X and Z are dependent
 
-        pvalue = cit_CIT.kci(0, 3, {2})
-        self.assertTrue(pvalue > 0.01) # X and Z are independent conditional on Y
+        pvalue = cit_CIT(0, 4, {2})
+        self.assertTrue(pvalue > 0.01)  # X and Z are independent conditional on Y
+
 
 if __name__ == '__main__':
     unittest.main()
