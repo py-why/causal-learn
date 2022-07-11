d6dc777f5bf3dac74852f3777b5a0fdf6ee52497
erdungao <erdun@erdungaodeMacBook-Pro.local>
2022-07-11 09:59:15 +0800

    Addding test plan

--- tests/TestANM.py
+++ tests/TestANM.py
@@ -20,7 +20,7 @@ class TestANM(unittest.TestCase):
         anm = ANM()
 
         # generate cause and noises
-        num_samples = 1000
+        num_samples = 3000
         x = np.random.randn(num_samples, 1)
         x = x / np.std(x)
         e = np.random.randn(num_samples, 1)
@@ -28,29 +28,27 @@ class TestANM(unittest.TestCase):
 
         # simulated data y = x + 2x^3 + e
         y_1 = x + 2 * x**3 + e
-        p_value_1, _ = anm.cause_or_effect(x, y_1)
-        self.assertTrue(p_value_1 > 0.05)
+        p_value_forward_1, p_value_backward_1 = anm.cause_or_effect(x, y_1)
+        self.assertTrue(p_value_forward_1 > 0.05)
+        self.assertTrue(p_value_backward_1 < 0.05)
 
         # simulated data y = 3*exp(x) + e
-        y_2 = 3 * np.exp(x) + e
-        p_value_2, _ = anm.cause_or_effect(x, y_2)
-        self.assertTrue(p_value_2 > 0.05)
+        y_2 = 5 * np.exp(x) + e
+        p_value_forward_2, p_value_backward_2 = anm.cause_or_effect(x, y_2)
+        self.assertTrue(p_value_forward_2 > 0.05)
+        self.assertTrue(p_value_backward_2 < 0.05)
 
         # simulated data y = 2 * Sigmoid(x) + e
-        y_3= 2/(1+np.exp(-x)) + e
-        p_value_3, _ = anm.cause_or_effect(x, y_3)
-        self.assertTrue(p_value_3 > 0.05)
+        y_3 = 5 / (1 + np.exp(-x)) + e
+        p_value_forward_3, p_value_backward_3 = anm.cause_or_effect(x, y_3)
+        self.assertTrue(p_value_forward_3 > 0.05)
+        self.assertTrue(p_value_backward_3 < 0.05)
 
     # data pair from the Tuebingen cause-effect pair dataset.
     def test_anm_pair(self):
 
         dataset = np.loadtxt('TestData/pair0001.txt')
         anm = ANM()
-        p_value_forward, _ = anm.cause_or_effect(dataset[:, 0].reshape(-1, 1), dataset[:, 1].reshape(-1, 1))
+        p_value_forward, p_value_backward = anm.cause_or_effect(dataset[:, 0].reshape(-1, 1), dataset[:, 1].reshape(-1, 1))
         self.assertTrue(p_value_forward > 0.05)
-
-
-if __name__ == '__main__':
-    test = TestANM()
-    test.test_anm_simulation()
-    test.test_anm_pair()
+        self.assertTrue(p_value_backward < 0.05)
