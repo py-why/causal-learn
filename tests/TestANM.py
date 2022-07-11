091b8dc914110f2b4cb689956ba9b371428cc251
erdungao <erdun@erdungaodeMacBook-Pro.local>
2022-07-11 10:20:41 +0800

    Adding p_value_th for easily checking.

--- tests/TestANM.py
+++ tests/TestANM.py
@@ -15,6 +15,8 @@ from causallearn.search.FCMBased.ANM.ANM import ANM
 class TestANM(unittest.TestCase):
 
     # Test ANM by some simulated data
+    p_value_th = 0.1
+
     def test_anm_simulation(self):
 
         anm = ANM()
@@ -29,20 +31,20 @@ class TestANM(unittest.TestCase):
         # simulated data y = x + 2x^3 + e
         y_1 = x + 2 * x**3 + e
         p_value_forward_1, p_value_backward_1 = anm.cause_or_effect(x, y_1)
-        self.assertTrue(p_value_forward_1 > 0.05)
-        self.assertTrue(p_value_backward_1 < 0.05)
+        self.assertTrue(p_value_forward_1 > self.p_value_th)
+        self.assertTrue(p_value_backward_1 < self.p_value_th)
 
         # simulated data y = 3*exp(x) + e
         y_2 = 5 * np.exp(x) + e
         p_value_forward_2, p_value_backward_2 = anm.cause_or_effect(x, y_2)
-        self.assertTrue(p_value_forward_2 > 0.05)
-        self.assertTrue(p_value_backward_2 < 0.05)
+        self.assertTrue(p_value_forward_2 > self.p_value_th)
+        self.assertTrue(p_value_backward_2 < self.p_value_th)
 
         # simulated data y = 2 * Sigmoid(x) + e
         y_3 = 5 / (1 + np.exp(-x)) + e
         p_value_forward_3, p_value_backward_3 = anm.cause_or_effect(x, y_3)
-        self.assertTrue(p_value_forward_3 > 0.05)
-        self.assertTrue(p_value_backward_3 < 0.05)
+        self.assertTrue(p_value_forward_3 > self.p_value_th)
+        self.assertTrue(p_value_backward_3 < self.p_value_th)
 
     # data pair from the Tuebingen cause-effect pair dataset.
     def test_anm_pair(self):
@@ -50,5 +52,5 @@ class TestANM(unittest.TestCase):
         dataset = np.loadtxt('TestData/pair0001.txt')
         anm = ANM()
         p_value_forward, p_value_backward = anm.cause_or_effect(dataset[:, 0].reshape(-1, 1), dataset[:, 1].reshape(-1, 1))
-        self.assertTrue(p_value_forward > 0.05)
-        self.assertTrue(p_value_backward < 0.05)
+        self.assertTrue(p_value_forward > self.p_value_th)
+        self.assertTrue(p_value_backward < self.p_value_th)
