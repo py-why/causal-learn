0ae6d6c1e317e49701e8530d538e2f8f6c205702
zhiyihuang <huangzhiyi.chn@gmail.com>
2022-08-13 17:32:03 +0800

    Refactored unit tests for FCI
    Signed-off-by: ZhiyiHuang <huangzhiyi.chn@gmail.com>

--- tests/TestFCI.py
+++ tests/TestFCI.py
@@ -1,17 +1,61 @@
+import hashlib
 import os
 import sys
 import time
-
-sys.path.append("")
 import unittest
 
+from networkx import DiGraph
 import numpy as np
 import pandas as pd
 
+from causallearn.graph.Dag import Dag
+from causallearn.graph.GraphNode import GraphNode
 from causallearn.search.ConstraintBased.FCI import fci
-from causallearn.utils.cit import chisq, fisherz, kci
+from causallearn.utils.cit import chisq, fisherz, kci, d_separation
+from causallearn.utils.DAG2PAG import dag2pag
 from causallearn.utils.GraphUtils import GraphUtils
 from causallearn.utils.PCUtils.BackgroundKnowledge import BackgroundKnowledge
+
+sys.path.append("")
+
+######################################### Test Notes ###########################################
+# All the benchmark results of loaded files (e.g. "./TestData/benchmark_returned_results/")    #
+# are obtained from the code of causal-learn as of commit                                      #
+# https://github.com/cmu-phil/causal-learn/commit/fb092d1 (08-04-2022).                        #
+#                                                                                              #
+# We are not sure if the results are completely "correct" (reflect ground truth graph) or not. #
+# So if you find your tests failed, it means that your modified code is logically inconsistent #
+# with the code as of fb092d1, but not necessarily means that your code is "wrong".            #
+# If you are sure that your modification is "correct" (e.g. fixed some bugs in fb092d1),       #
+# please report it to us. We will then modify these benchmark results accordingly. Thanks :)   #
+######################################### Test Notes ###########################################
+
+BENCHMARK_TXTFILE_TO_MD5 = {
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_asia_fci_chisq_0.05.txt": "65f54932a9d8224459e56c40129e6d8b",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_cancer_fci_chisq_0.05.txt": "0312381641cb3b4818e0c8539f74e802",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_earthquake_fci_chisq_0.05.txt": "a1160b92ce15a700858552f08e43b7de",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_sachs_fci_chisq_0.05.txt": "c4a0d5eaf793838d6ad2b58632ba0ded",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_survey_fci_chisq_0.05.txt": "b1a28eee1e0c6ea8a64ac1624585c3f4",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_alarm_fci_chisq_0.05.txt": "c3bbc2b8aba456a4258dd071a42085bc",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_barley_fci_chisq_0.05.txt": "ccbd38b245ced2ac7e632415b93cbef1",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_child_fci_chisq_0.05.txt": "6b7858589e12f04b0f489ba4589a1254",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_insurance_fci_chisq_0.05.txt": "9975942b936aa2b1fc90c09318ca2d08",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_water_fci_chisq_0.05.txt": "48eee804d59526187b7ecd0519556ee5",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_hailfinder_fci_chisq_0.05.txt": "6b9a6b95b6474f8530e85c022f4e749c",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_hepar2_fci_chisq_0.05.txt": "e9de65c752011d72c36589b68590011d",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_win95pts_fci_chisq_0.05.txt": "648fdf271e1440c06ca2b31b55ef1f3f",
+    "tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_andes_fci_chisq_0.05.txt": "939375acd3c623334b20c709bf8f347c",
+    "tests/TestData/benchmark_returned_results/linear_10_fci_fisherz_0.05.txt": "289c86f9c665bf82bbcc4c9e1dcec3e7"
+}
+
+INCONSISTENT_RESULT_GRAPH_ERRMSG = "Returned graph is inconsistent with the benchmark. Please check your code with the commit fb092d1."
+INCONSISTENT_RESULT_GRAPH_WITH_PAG_ERRMSG = "Returned graph is inconsistent with the truth PAG."
+
+# verify files integrity first
+for file_path, expected_MD5 in BENCHMARK_TXTFILE_TO_MD5.items():
+    with open(file_path, 'rb') as fin:
+        assert hashlib.md5(fin.read()).hexdigest() == expected_MD5,\
+            f'{file_path} is corrupted. Please download it again from https://github.com/cmu-phil/causal-learn/blob/fb092d1/tests/TestData'
 
 
 def gen_coef():
@@ -19,77 +63,125 @@ def gen_coef():
 
 
 class TestFCI(unittest.TestCase):
-
     def test_simple_test(self):
-        np.random.seed(0)
-        sample_size, loc, scale = 200, 0.0, 1.0
-        X1 = np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X2 = X1 * gen_coef() + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X3 = X1 * gen_coef() + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X4 = X2 * gen_coef() + X3 * gen_coef() + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        data = np.array([X1, X2, X3, X4]).T
-        G, edges = fci(data, fisherz, 0.05, verbose=False)
-        # pdy = GraphUtils.to_pydot(G)
-        # pdy.write_png('simple_test.png')
+        data = np.empty(shape=(0, 4))
+        true_dag = DiGraph()
+        true_dag.add_edges_from([(0, 1), (0, 2), (1, 3), (2, 3)])
+        G, edges = fci(data, d_separation, 0.05, verbose=False, true_dag=true_dag)
+
+        ground_truth_nodes = []
+        for i in range(4):
+            ground_truth_nodes.append(GraphNode(f'X{i+1}'))
+        ground_truth_dag = Dag(ground_truth_nodes)
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[0], ground_truth_nodes[1])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[0], ground_truth_nodes[2])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[1], ground_truth_nodes[3])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[2], ground_truth_nodes[3])
+        pag = dag2pag(ground_truth_dag, [])
+
+        print(f'fci(data, d_separation, 0.05):')
+        self.run_simulate_data_test(pag, G)
 
         nodes = G.get_nodes()
         assert G.is_adjacent_to(nodes[0], nodes[1])
 
         bk = BackgroundKnowledge().add_forbidden_by_node(nodes[0], nodes[1]).add_forbidden_by_node(nodes[1], nodes[0])
-        G_with_background_knowledge, edges = fci(data, fisherz, 0.05, verbose=True, background_knowledge=bk)
+        G_with_background_knowledge, edges = fci(data, d_separation, 0.05, verbose=False, true_dag=true_dag, background_knowledge=bk)
         assert not G_with_background_knowledge.is_adjacent_to(nodes[0], nodes[1])
 
     def test_simple_test2(self):
-        np.random.seed(0)
-        sample_size, loc, scale = 2000, 0.0, 1.0
-        T1 = np.random.normal(loc=loc, scale=scale, size=sample_size)
-        T2 = np.random.normal(loc=loc, scale=scale, size=sample_size)
-        C = np.random.normal(loc=loc, scale=scale, size=sample_size)
-        F = C * gen_coef() + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        H = C * gen_coef() + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        B = F * gen_coef() + T1 * gen_coef() + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        D = H * gen_coef() + T2 * gen_coef() + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        A = D * gen_coef() + T1 * gen_coef() + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        E = B * gen_coef() + T2 * gen_coef() + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        data = np.array([A, B, C, D, E, F, H]).T
-        G, edges = fci(data, fisherz, 0.05, verbose=True)
-        # pdy = GraphUtils.to_pydot(G)
-        # pdy.write_png('simple_test_2.png')
-        print(G)
+        data = np.empty(shape=(0, 7))
+        true_dag = DiGraph()
+        true_dag.add_edges_from([('L1', 0), ('L1', 1), ('L2', 3), ('L2', 4), (2, 5),
+                                 (2, 6), (5, 1), (6, 3), (3, 0), (1, 4)])
+        G, edges = fci(data, d_separation, 0.05, verbose=False, true_dag=true_dag)
+        ground_truth_nodes = []
+        for i in range(9):
+            ground_truth_nodes.append(GraphNode(f'X{i+1}'))
+        ground_truth_dag = Dag(ground_truth_nodes)
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[7], ground_truth_nodes[0])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[7], ground_truth_nodes[1])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[8], ground_truth_nodes[3])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[8], ground_truth_nodes[4])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[2], ground_truth_nodes[5])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[2], ground_truth_nodes[6])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[5], ground_truth_nodes[1])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[6], ground_truth_nodes[3])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[3], ground_truth_nodes[0])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[1], ground_truth_nodes[4])
+
+        pag = dag2pag(ground_truth_dag, ground_truth_nodes[7: 9])
+
+        print(f'fci(data, d_separation, 0.05):')
+        self.run_simulate_data_test(pag, G)
+
 
     def test_simple_test3(self):
-        np.random.seed(0)
-        sample_size, loc, scale = 2000, 0.0, 1.0
-        X1 = np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X2 = np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X3 = X1 * gen_coef() + X2 * gen_coef() + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X4 = X3 * gen_coef() + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X5 = X3 * gen_coef() + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        data = np.array([X1, X2, X3, X4, X5]).T
-        G, edges = fci(data, fisherz, 0.05, verbose=True)
-        # pdy = GraphUtils.to_pydot(G, edges)
-        # pdy.write_png('simple_test3.png')
+
+        data = np.empty(shape=(0, 5))
+        true_dag = DiGraph()
+        true_dag.add_edges_from([(0, 2), (1, 2), (2, 3), (2, 4)])
+        G, edges = fci(data, d_separation, 0.05, verbose=False, true_dag=true_dag)
+
+        ground_truth_nodes = []
+        for i in range(5):
+            ground_truth_nodes.append(GraphNode(f'X{i + 1}'))
+        ground_truth_dag = Dag(ground_truth_nodes)
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[0], ground_truth_nodes[2])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[1], ground_truth_nodes[2])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[2], ground_truth_nodes[3])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[2], ground_truth_nodes[4])
+
+        pag = dag2pag(ground_truth_dag, [])
+
+        print(f'fci(data, d_separation, 0.05):')
+        self.run_simulate_data_test(pag, G)
+
 
     def test_fritl(self):
-        np.random.seed(0)
-        sample_size, loc, scale = 1000, 0.0, 1.0
-        L1 = np.random.normal(loc=loc, scale=scale, size=sample_size)
-        L2 = np.random.normal(loc=loc, scale=scale, size=sample_size)
-        L3 = np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X1 = gen_coef() * L1 + gen_coef() * L2 + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X2 = gen_coef() * X1 + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X3 = gen_coef() * X2 + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X4 = gen_coef() * X1 + gen_coef() * L3 + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X5 = gen_coef() * X3 + gen_coef() * L3 + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X6 = gen_coef() * L1 + np.random.normal(loc=loc, scale=scale, size=sample_size)
-        X7 = gen_coef() * L2 + gen_coef() * L3 + gen_coef() * X6 + np.random.normal(loc=loc, scale=scale,
-                                                                                    size=sample_size)
-        data = np.array([X1, X2, X3, X4, X5, X6, X7]).T
+        data = np.empty(shape=(0, 7))
+        true_dag = DiGraph()
+        true_dag.add_edges_from([('L1', 0), ('L1', 5), ('L2', 0), ('L2', 6), ('L3', 3), ('L3', 4), ('L3', 6),
+                                 (0, 1), (0, 2), (1, 2), (2, 4), (5, 6)])
+        G, edges = fci(data, d_separation, 0.05, verbose=False, true_dag=true_dag)
 
-        G, edges = fci(data, fisherz, 0.05, verbose=True)
-        # pdy = GraphUtils.to_pydot(G)
-        # pdy.write_png('fritl.png')
-        print(G)
+        ground_truth_nodes = []
+        for i in range(10):
+            ground_truth_nodes.append(GraphNode(f'X{i + 1}'))
+        ground_truth_dag = Dag(ground_truth_nodes)
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[7], ground_truth_nodes[0])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[7], ground_truth_nodes[5])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[8], ground_truth_nodes[0])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[8], ground_truth_nodes[6])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[9], ground_truth_nodes[3])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[9], ground_truth_nodes[4])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[9], ground_truth_nodes[6])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[0], ground_truth_nodes[1])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[0], ground_truth_nodes[2])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[1], ground_truth_nodes[2])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[2], ground_truth_nodes[4])
+        ground_truth_dag.add_directed_edge(ground_truth_nodes[5], ground_truth_nodes[6])
+
+        pag = dag2pag(ground_truth_dag, ground_truth_nodes[7: 10])
+
+        print(f'fci(data, d_separation, 0.05):')
+        self.run_simulate_data_test(pag, G)
+
+
+    @staticmethod
+    def run_simulate_data_test(truth, est):
+        graph_utils = GraphUtils()
+        adj_precision = graph_utils.adj_precision(truth, est)
+        adj_recall = graph_utils.adj_recall(truth, est)
+        arrow_precision = graph_utils.arrow_precision(truth, est)
+        arrow_recall = graph_utils.adj_precision(truth, est)
+
+        print(f'adj_precision: {adj_precision}')
+        print(f'adj_recall: {adj_recall}')
+        print(f'arrow_precision: {arrow_precision}')
+        print(f'arrow_recall: {arrow_recall}')
+        print()
+        assert np.isclose([adj_precision, adj_recall, arrow_precision, arrow_recall], [1.0, 1.0, 1.0, 1.0]).all()
 
     def test_bnlearn_discrete_datasets(self):
         benchmark_names = [
@@ -99,13 +191,17 @@ class TestFCI(unittest.TestCase):
             "andes"
         ]
 
-        bnlearn_path = './TestData/bnlearn_discrete_10000/data'
+        bnlearn_path = 'tests/TestData/bnlearn_discrete_10000/data'
         for bname in benchmark_names:
             data = np.loadtxt(os.path.join(bnlearn_path, f'{bname}.txt'), skiprows=1)
             G, edges = fci(data, chisq, 0.05, verbose=False)
-            print('finish')
+            benchmark_returned_graph = np.loadtxt(
+                f'tests/TestData/benchmark_returned_results/bnlearn_discrete_10000_{bname}_fci_chisq_0.05.txt')
+            assert np.all(G.graph == benchmark_returned_graph), INCONSISTENT_RESULT_GRAPH_ERRMSG
 
     def test_continuous_dataset(self):
-        data = np.loadtxt('./data_linear_10.txt', skiprows=1)
+        data = np.loadtxt('tests/data_linear_10.txt', skiprows=1)
         G, edges = fci(data, fisherz, 0.05, verbose=False)
-        print('finish')
+        benchmark_returned_graph = np.loadtxt(
+            f'tests/TestData/benchmark_returned_results/linear_10_fci_fisherz_0.05.txt')
+        assert np.all(G.graph == benchmark_returned_graph), INCONSISTENT_RESULT_GRAPH_ERRMSG
