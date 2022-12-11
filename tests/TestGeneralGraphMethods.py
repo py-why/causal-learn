ce4aa694a2c06a6dc7fb386081f891dd186fbe3f
zhiyihuang <huangzhiyi.chn@gmail.com>
2022-12-11 14:23:24 +0800

    Added a test for GeneralGraph
    Signed-off-by: wean2016 <weanyq@gmail.com>
    Signed-off-by: ZhiyiHuang <huangzhiyi.chn@gmail.com>

--- tests/TestGeneralGraphMethods.py
+++ tests/TestGeneralGraphMethods.py
@@ -25,3 +25,25 @@ class TestGeneralGraphMethods(unittest.TestCase):
 
         dag.set_nodes(new_nodes)
         self.assertEqual(dag.get_nodes(), new_nodes)
+
+    def test_is_ancestor_of(self):
+        nodes = [GraphNode(str(i)) for i in range(0, 5)]
+        graph = GeneralGraph(nodes)
+        no_of_var = len(nodes)
+        for i in range(no_of_var):
+            for j in range(i + 1, no_of_var):
+                graph.add_edge(Edge(nodes[i], nodes[j], Endpoint.TAIL, Endpoint.TAIL))
+
+        for i in range(no_of_var):
+            for j in range(i + 1, no_of_var):
+                edge = graph.get_edge(nodes[i], nodes[j])
+                graph.remove_edge(edge)
+
+        graph.add_edge(Edge(nodes[0], nodes[3], Endpoint.TAIL, Endpoint.ARROW))
+        graph.add_edge(Edge(nodes[1], nodes[3], Endpoint.TAIL, Endpoint.ARROW))
+        graph.add_edge(Edge(nodes[3], nodes[2], Endpoint.TAIL, Endpoint.ARROW))
+        graph.add_edge(Edge(nodes[3], nodes[4], Endpoint.TAIL, Endpoint.ARROW))
+
+
+        assert graph.is_ancestor_of(nodes[3], nodes[2])
+        assert not graph.is_ancestor_of(nodes[2], nodes[3])
